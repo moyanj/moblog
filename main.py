5d97886888a0c860805689ddedcc18be639d02ee
moyan <1561515308@qq.com>
2025-05-31 18:31:30 +0800

    feat: 添加文件

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from moblog!")
-
+import uvicorn
+import sys
 
 if __name__ == "__main__":
-    main()
+    reload = True
+    worker = 1
+    if sys.argv[1] == "rel":
+        reload = False
+        worker = 24
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=reload, workers=worker)
