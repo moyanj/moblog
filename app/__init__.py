0a6106a6420b7bcda6b216a4506897eea0deca79
moyan <1561515308@qq.com>
2025-06-06 10:34:36 +0800

    refactor(app): 重构异常处理和路由注册
    - 添加自定义异常处理器，统一处理 HTTP 异常和请求验证异常
    - 移动路由注册至 lifespan 函数中，优化应用启动逻辑
    - 引入 Response 工具类，用于统一响应格式

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from . import db
 from .config import server_config
 from .routers import auth_router, setting_router
+from .utils import Response
 
 
 @asynccontextmanager
@@ -15,6 +18,10 @@ async def lifespan(app: FastAPI):
     logger.info(f"Running at {server_config.host}:{server_config.port}")
     await db.init_db()
     logger.info("Started successfully")
+
+    app.include_router(setting_router)
+    app.include_router(auth_router)
+
     yield
     await db.Tortoise.close_connections()
 
@@ -29,8 +36,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(setting_router)
-app.include_router(auth_router)
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+    return Response(message=exc.detail, status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return Response(data=exc.errors(), message="Request is Invalid", status_code=400)
 
 
 @app.get("/")
